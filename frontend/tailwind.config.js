d609747d6dbf221bfee80d96747087dff102874f
Yeoungmin Shin <yshin22@gmu.edu>
2024-04-16 16:47:45 -0400

    Tailwind setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        "twitter-blue": "#1DA1F2",
+      },
+    },
+  },
+  plugins: [],
+}
+
