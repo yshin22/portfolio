e708bb7aaf2a9e2f83b373fbec1d3ff58ba61d7f
Yeoungmin Shin <scarh54@gmail.com>
2024-11-30 20:49:41 -0500

    add env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-const db =
-  "mongodb+srv://scarh54:LcIbCECZCJ6wa9m8@portfolio.my5ks.mongodb.net/?retryWrites=true&w=majority&appName=Portfolio";
+
 /* Replace <password> with your database password */
 
 mongoose.set("strictQuery", true, "useNewUrlParser", true);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB is Connected...");
   } catch (err) {
     console.error(err.message);
--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,25 @@ const connectDB = require('./config/db');
 const routes = require("./routes/api/book");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
 
+// CORS configuration
 app.use(cors({origin: true, credentials: true}));
 
+// Body Parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Routes
 app.use("/api/book", routes);
 
+// Connect to Database
 connectDB();
 
+// Basic route
 app.get('/', (req, res) => res.send('Hello world!'));
 
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Vercel serverless function exports
+module.exports = app;
